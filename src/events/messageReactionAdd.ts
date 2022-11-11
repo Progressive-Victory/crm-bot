afd4e1971519b73a6ad6221eadaf6b4ff233a61c
Lewdcario <isabellafj97@gmail.com>
2022-11-10 21:49:31 -0600

    fix reaction user tracking event

--- src/events/messageReactionAdd.ts
+++ src/events/messageReactionAdd.ts
@@ -4,6 +4,8 @@ import { onConnect } from '../structures/helpers';
 export default async function onMessageReactionAdd(reaction: MessageReaction, user: User) {
 	if (reaction.message.inGuild() && process.env.VERIFY_EMOJI === reaction.emoji.name && !reaction.message.content) {
 		if (!reaction.message.author) await reaction.message.fetch();
-		await onConnect(reaction.message.author.id, reaction.message.author.tag, user.id, user.tag, reaction.message.guildId, reaction.message.channelId);
+		const users  = await reaction.users.fetch();
+		const otherUser = users.find(u => u.id !== user.id) || user;
+		await onConnect(reaction.message.author.id, reaction.message.author.tag, otherUser.id, otherUser.tag, reaction.message.guildId, reaction.message.channelId);
 	}
 }
