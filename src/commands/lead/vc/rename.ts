2d0943db81cdaa6ecf38419870a26b5e7ca33731
Isabella <isabellafj97@gmail.com>
2023-03-23 01:11:05 -0500

    Minor Commands Rework (#34)

--- src/commands/lead/vc/rename.ts
+++ src/commands/lead/vc/rename.ts
@@ -6,32 +6,35 @@ import {
 	Snowflake,
 	VoiceChannel
 } from 'discord.js';
-import { VCChannelIDs } from '../../structures/Constants';
-import { State } from '../../declarations/states';
-import { Command } from '../../structures/Command';
+import { VCChannelIDs } from '../../../structures/Constants';
+import { State } from '../../../declarations/states';
+import { Command } from '../../../structures/Command';
 
 const states = Object.values(State);
 
 async function execute(interaction: ChatInputCommandInteraction<'cached'>): Promise<InteractionResponse<boolean>> {
-	let reply:string;
-	const allowedChannels: Snowflake[] = VCChannelIDs;
 	const channel = interaction.options.getChannel('channel', true) as VoiceChannel;
+
+	if (!channel.permissionsFor(interaction.client.user).has('ManageChannels')) {
+		return interaction.reply({ ephemeral: true, content: `I do not have permission to manage ${channel}.` });
+	}
+
+	let reply: string;
+	const allowedChannels: Snowflake[] = VCChannelIDs;
 	const name = interaction.options.getString('name', true);
+	const stateLead = interaction.member;
 
 	if (!allowedChannels.includes(channel.id)) {
-		reply = `You are not allowed to rename ${channel}`;
+		reply = `You are not allowed to rename ${channel}. However, you can rename any of the following channels: ${allowedChannels.map((id) => `<#${id}>`).join(', ')}.`;
 	}
 	else {
-		if (!channel.permissionsFor(interaction.client.user).has('ManageChannels')) {
-			return interaction.reply({ ephemeral: true, content: `I do not have permission to manage ${channel}.` });
-		}
-
-		await channel.setName(name, 'State Lead has renamed this channel');
-		reply = `${channel} has been Successfully renamed`;
+		await channel.setName(name, `${stateLead.user.tag} renamed ${channel.name}`);
+		reply = `${channel} has been successfully renamed!`;
 	}
 
 	return interaction.reply({ ephemeral: true, content: reply });
 }
+
 async function autocomplete(interaction: AutocompleteInteraction) {
 	const member = interaction.member as GuildMember;
 	const stateRole = member.roles.valueOf().find((role) => states.includes(role.name as State));
@@ -43,8 +46,10 @@ async function autocomplete(interaction: AutocompleteInteraction) {
 		filtered.map((choice) => ({ name: choice, value: choice })).slice(0, 14)
 	);
 }
+
 export default new Command({
 	execute,
 	autocomplete,
-	name: 'state-lead'
+	name: 'lead',
+	group: 'vc'
 });
