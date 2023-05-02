00d8eaa7f9ca5b94672dce81c0eb854306eb33a2
Isabella <isabellafj97@gmail.com>
2023-05-01 20:09:53 -0500

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,53 @@
-import { join } from 'path';
-
+import {
+	DiscordjsError, GatewayIntentBits as Intents, Partials
+} from 'discord.js';
 import { config } from 'dotenv';
-import { GatewayIntentBits, Partials } from 'discord.js';
+import path from 'path';
+import './i18n';
+import ExtendedClient from './structures/Client';
 
-import CustomClient from './structures/Client';
-import prototypes from './structures/prototypes';
-import { CustomClientOptions } from './declarations/typings';
-
+// Load .env file contents
 config();
-prototypes();
 
-const clientOptions: CustomClientOptions = {
-	token: process.env.TOKEN,
-	eventsDir: join(__dirname, 'events'),
-	commandsDir: join(__dirname, 'commands'),
-	interactionsDir: join(__dirname, 'interactions'),
-	partials: [Partials.Reaction, Partials.User, Partials.Channel, Partials.Message],
+// Initialization (specify intents and partials)
+const client = new ExtendedClient({
 	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMembers,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		// Analytics & Metrics
-		GatewayIntentBits.GuildMessageReactions,
-		GatewayIntentBits.GuildVoiceStates
-	]
-};
-
-const client = new CustomClient(clientOptions);
-
-client.launch();
+		Intents.Guilds,
+		Intents.GuildMessages,
+		Intents.GuildVoiceStates,
+		Intents.MessageContent,
+		Intents.GuildMembers,
+		Intents.GuildModeration
+	],
+	partials: [
+		Partials.Message,
+		Partials.Channel,
+		Partials.Reaction,
+		Partials.GuildMember
+	],
+	eventsPath: path.join(__dirname, 'events'),
+	commandPath: path.join(__dirname, 'commands'),
+	contextMenuPath: path.join(__dirname, 'context_menus'),
+	buttonPath: path.join(__dirname, 'interactions', 'buttons'),
+	selectMenuPath: path.join(__dirname, 'interactions', 'select_menus'),
+	modalPath: path.join(__dirname, 'interactions', 'modals'),
+	restVersion: '10',
+	receiveMessageComponents: true,
+	receiveModals: true,
+	receiveAutocomplete: true,
+	replyOnError: true,
+	splitCustomId: true,
+	splitCustomIdOn: '_',
+	useGuildCommands: false
+});
+client.login(process.env.TOKEN)
+	.catch((err: unknown) => {
+		if (err instanceof DiscordjsError) {
+			if (err.code === 'TokenMissing') console.warn(`\n[Error] ${err.name}: ${err.message} Did you create a .env file?\n`);
+			else if (err.code === 'TokenInvalid') console.warn(`\n[Error] ${err.name}: ${err.message} Check your .env file\n`);
+			else throw err;
+		}
+		else {
+			throw err;
+		}
+	});
