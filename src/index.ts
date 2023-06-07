c61b253ea603ab5dba0d0959fd19def30de16552
Isabella <isabellafj97@gmail.com>
2023-06-06 19:14:20 -0500

    stuff

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import 'module-alias/register';
 import { Client } from '@Client';
 import { init } from '@i18n';
 import {
-	GatewayIntentBits as Intents, Locale, Partials
+	GatewayIntentBits as Intents, Locale, Partials 
 } from 'discord.js';
 import { config } from 'dotenv';
 import { join } from 'path';
