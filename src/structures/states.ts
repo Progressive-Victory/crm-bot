162c1a09cf5ac7ab64ad40c70e67b2504c301f2f
John William Bowen <johnwb1776@gmail.com>
2023-06-17 18:53:11 -0400

    remove aysnc function

--- src/structures/states.ts
+++ src/structures/states.ts
@@ -128,8 +128,8 @@ export type StateAbbreviation =
 export const states = new Collection<StateAbbreviation, state>();
 statesConfig.map((s) => states.set(s.abbreviation.toLocaleLowerCase() as StateAbbreviation, s));
 
-export async function isMemberStateLead(member: GuildMember) {
-	const role = await member.guild.roles.fetch(stateLeadRoleID);
+export function isMemberStateLead(member: GuildMember) {
+	const role = member.guild.roles.cache.get(stateLeadRoleID);
 	if (!role) throw Error('Missing Valid STATE_LEAD_ROLE_ID in .env');
 	return member.roles.cache.has(stateLeadRoleID);
 }
