1c793fdcf264ab1b94d071a9d2330ed8f2895716
John William Bowen <johnwb1776@gmail.com>
2023-09-16 22:14:25 -0400

    Log-out channel messages

--- src/events/channelDelete.ts
+++ src/events/channelDelete.ts
@@ -1,17 +1,26 @@
 import { Event, Logger } from '@Client';
 import {
-	ChannelType, DMChannel, Events, GuildChannel, TextChannel 
+	AttachmentBuilder, ChannelType, DMChannel, Events, GuildChannel, TextBasedChannel, TextChannel, VoiceChannel 
 } from 'discord.js';
+import { printChannel } from 'src/structures/print-channel';
 
 const parentId = process.env.EVENT_CATEGORY_ID;
+const eventLogId = process.env.EVENT_LOG_CHANNEL_ID;
 
 async function execute(channel: DMChannel | GuildChannel) {
 	if (channel.isDMBased()) return;
-	if (channel.type === ChannelType.GuildText && channel.parentId === parentId) {
+	const files: AttachmentBuilder[] = [];
+	const {
+		type, guild, id 
+	} = channel;
+
+	if (type === ChannelType.GuildText && channel.parentId === parentId) {
 		const textChannel = channel as TextChannel;
-		const event = channel.guild.scheduledEvents.cache.find((_e, k) => k === textChannel.topic.split(':')[1]);
+		const event = guild.scheduledEvents.cache.find((_e, k) => k === textChannel.topic.split(':')[1]);
+		files.push(await printChannel(channel as TextChannel));
 		try {
 			if (event && event.channel) {
+				files.push(await printChannel(event.channel as VoiceChannel));
 				event.channel.delete('Event channel deleted');
 			}
 		}
@@ -19,16 +28,24 @@ async function execute(channel: DMChannel | GuildChannel) {
 			Logger.error(error, 'Event was Deleted');
 		}
 	}
-	if (channel.type === ChannelType.GuildVoice && channel.parentId === parentId) {
-		const event = channel.guild.scheduledEvents.cache.find((e) => e.channelId === channel.id);
+	if (type === ChannelType.GuildVoice && channel.parentId === parentId) {
+		const event = guild.scheduledEvents.cache.find((e) => e.channelId === id);
+		files.push(await printChannel(channel as VoiceChannel));
 		if (event) {
-			const textChannel = channel.guild.channels.cache.find(
+			const textChannel = guild.channels.cache.find(
 				(c) => c.parentId === parentId && c.type === ChannelType.GuildText && (c as TextChannel).topic.split(':')[1] === event.id
 			);
 			if (textChannel) {
+				files.push(await printChannel(textChannel as TextChannel));
 				textChannel.delete('Event Deleted');
 			}
 		}
+	}
+	if (files) {
+		const eventLogChannel = guild.channels.cache.find(
+			(c, k) => k === eventLogId && (c.type === ChannelType.GuildText || c.type === ChannelType.PublicThread)
+		) as TextBasedChannel;
+		eventLogChannel.send({});
 	}
 }
 
--- src/events/guildScheduledEventDelete.ts
+++ src/events/guildScheduledEventDelete.ts
@@ -1,19 +1,35 @@
 import { Event, Logger } from '@Client';
 import {
-	ChannelType, Events, GuildScheduledEvent, TextChannel 
+	AttachmentBuilder, ChannelType, Events, GuildScheduledEvent, TextBasedChannel, TextChannel, VoiceChannel 
 } from 'discord.js';
+import { printChannel } from 'src/structures/print-channel';
+
+const parentId = process.env.EVENT_CATEGORY_ID;
+const eventLogId = process.env.EVENT_LOG_CHANNEL_ID;
 
 async function execute(guildScheduledEvent: GuildScheduledEvent) {
-	const parentId = process.env.EVENT_CATEGORY_ID;
 	const eventTextChannel = guildScheduledEvent.guild.channels.cache.find(
 		(c) => c.parentId === parentId && c.type === ChannelType.GuildText && (c as TextChannel).topic.split(':')[1] === guildScheduledEvent.id
 	);
+
+	const eventLogChannel = guildScheduledEvent.guild.channels.cache.find(
+		(c, k) => k === eventLogId && (c.type === ChannelType.GuildText || c.type === ChannelType.PublicThread)
+	) as TextBasedChannel;
+	const files: AttachmentBuilder[] = [];
 	try {
 		if (eventTextChannel) {
+			files.push(await printChannel(eventTextChannel as TextChannel));
 			eventTextChannel.delete('Event Deleted');
 		}
 		if (guildScheduledEvent.channel.parentId === parentId) {
+			files.push(await printChannel(guildScheduledEvent.channel as VoiceChannel));
 			guildScheduledEvent.channel.delete('Event Deleted');
+		}
+		if (files) {
+			eventLogChannel.send({
+				content: 'Log for meesages sent during event',
+				files
+			});
 		}
 	}
 	catch (error) {
