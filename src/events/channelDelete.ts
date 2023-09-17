04bfc13e95fa114f632e2bef0fcf546befb664ab
John William Bowen <johnwb1776@gmail.com>
2023-09-16 22:34:19 -0400

    remove print channel from channel delete

--- src/events/channelDelete.ts
+++ src/events/channelDelete.ts
@@ -1,15 +1,12 @@
 import { Event, Logger } from '@Client';
 import {
-	AttachmentBuilder, ChannelType, DMChannel, Events, GuildChannel, TextBasedChannel, TextChannel, VoiceChannel 
+	ChannelType, DMChannel, Events, GuildChannel, TextChannel 
 } from 'discord.js';
-import { printChannel } from 'src/structures/print-channel';
 
 const parentId = process.env.EVENT_CATEGORY_ID;
-const eventLogId = process.env.EVENT_LOG_CHANNEL_ID;
 
 async function execute(channel: DMChannel | GuildChannel) {
 	if (channel.isDMBased()) return;
-	const files: AttachmentBuilder[] = [];
 	const {
 		type, guild, id 
 	} = channel;
@@ -17,10 +14,8 @@ async function execute(channel: DMChannel | GuildChannel) {
 	if (type === ChannelType.GuildText && channel.parentId === parentId) {
 		const textChannel = channel as TextChannel;
 		const event = guild.scheduledEvents.cache.find((_e, k) => k === textChannel.topic.split(':')[1]);
-		files.push(await printChannel(channel as TextChannel));
 		try {
 			if (event && event.channel) {
-				files.push(await printChannel(event.channel as VoiceChannel));
 				event.channel.delete('Event channel deleted');
 			}
 		}
@@ -30,22 +25,14 @@ async function execute(channel: DMChannel | GuildChannel) {
 	}
 	if (type === ChannelType.GuildVoice && channel.parentId === parentId) {
 		const event = guild.scheduledEvents.cache.find((e) => e.channelId === id);
-		files.push(await printChannel(channel as VoiceChannel));
 		if (event) {
 			const textChannel = guild.channels.cache.find(
 				(c) => c.parentId === parentId && c.type === ChannelType.GuildText && (c as TextChannel).topic.split(':')[1] === event.id
 			);
 			if (textChannel) {
-				files.push(await printChannel(textChannel as TextChannel));
 				textChannel.delete('Event Deleted');
 			}
 		}
-	}
-	if (files) {
-		const eventLogChannel = guild.channels.cache.find(
-			(c, k) => k === eventLogId && (c.type === ChannelType.GuildText || c.type === ChannelType.PublicThread)
-		) as TextBasedChannel;
-		eventLogChannel.send({});
 	}
 }
 
