89f09814fc86ec3e665de21dd09186c0780e357b
John William Bowen <johnwb1776@gmail.com>
2023-10-04 06:54:32 -0400

    updated command interface

--- src/Client/Command.ts
+++ src/Client/Command.ts
@@ -21,22 +21,33 @@ export type ChatInputCommandBuilders =
 /**
  * Slash command or context command
  */
-export interface Command {
+export interface Command<
+	TypeBuilder extends ChatInputCommandBuilders | ContextMenuCommandBuilder,
+	TypeInteraction extends ChatInputCommandInteraction | ContextMenuCommandInteraction
+> {
 	/**
 	 * The constructor for the registration for the command
 	 */
-	builder: ChatInputCommandBuilders | ContextMenuCommandBuilder;
+	readonly builder: TypeBuilder;
 
 	/**
 	 * State if the command is available in all servers
 	 */
-	isGlobal: boolean;
+	readonly isGlobal: boolean;
+
+	execute(interaction: TypeInteraction): Promise<ReturnableInteraction> | ReturnableInteraction;
+
+	setBuilder(input: TypeBuilder | ((subcommandBuilder: TypeBuilder) => TypeBuilder)): this;
+
+	setGlobal(isGlobal: boolean): this;
+
+	setExecute(execute: (interaction: TypeInteraction) => Promise<ReturnableInteraction> | ReturnableInteraction): this;
 }
 
 /**
  * Slash command
  */
-export class ChatInputCommand implements Command {
+export class ChatInputCommand implements Command<ChatInputCommandBuilders, ChatInputCommandInteraction> {
 	readonly builder: ChatInputCommandBuilders;
 
 	readonly isGlobal: boolean;
@@ -85,7 +96,7 @@ export class ChatInputCommand implements Command {
 	}
 }
 
-export class ContextMenuCommand implements Command {
+export class ContextMenuCommand implements Command<ContextMenuCommandBuilder, ContextMenuCommandInteraction> {
 	readonly builder: ContextMenuCommandBuilder;
 
 	readonly isGlobal: boolean;
