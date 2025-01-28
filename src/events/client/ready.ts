5cb30b84900e0e326d8648a3e471bb7906662694
antacid3 <antacid3>
2025-01-27 21:45:18 -0500

    Restrict Event's execute property type

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,15 +1,15 @@
-import { Events } from 'discord.js';
-import { Client, Event } from '../../Classes/index.js';
+import { Client, Events } from 'discord.js';
+import { Event } from '../../Classes/index.js';
 
 /**
  * function to run on the client ready event
  * @param client client object
  */
-async function onReady(client: Client) {
+async function onReady(client: Client<true>) {
 	console.log(`Ready! Logged in as ${client.user.tag}`);
 }
 
-export default new Event()
+export default new Event<Events.ClientReady>()
 	.setName(Events.ClientReady)
 	.setOnce(true)
 	.setExecute(onReady);
