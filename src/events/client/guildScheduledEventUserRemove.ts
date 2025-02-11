2b3caa308f7bf4db73f70ebac28d3ebc824da735
antacid3 <antacid3>
2025-02-10 21:21:29 -0500

    Add persistence logic

--- src/events/client/guildScheduledEventUserRemove.ts
+++ src/events/client/guildScheduledEventUserRemove.ts
@@ -1,9 +1,14 @@
 import { Events, GuildScheduledEvent, PartialGuildScheduledEvent, User } from 'discord.js';
 import { Event } from '../../Classes/index.js';
+import { ScheduledEventInterest } from '../../features/attendence/index.js';
 
 export default new Event({
 	name: Events.GuildScheduledEventUserRemove,
-	execute: (guildScheduledEvent: GuildScheduledEvent | PartialGuildScheduledEvent, user: User) => {
-		console.log(`${user.username} has rescinded interest in ${guildScheduledEvent.name}`)
+	execute: async (guildScheduledEvent: GuildScheduledEvent | PartialGuildScheduledEvent, user: User) => {
+		const now = new Date();
+		for await (const e of ScheduledEventInterest.find({eventId: oldGuildScheduledEvent.id, endedAt: null})) {
+			e.endedAt = now;
+			e.save();
+		}
 	},
 });
--- src/features/attendence/index.ts
+++ src/features/attendence/index.ts
@@ -15,5 +15,7 @@ export const ActivatedEvent = model('ActivatedEvent', new Schema({
 export const ScheduledEventInterest = model('ScheduledEventInterest', new Schema({
 	userId: { type: String, required: true },
 	displayName: { type: String, required: true },
+	eventId: { type: String, required: true },
+	eventName: { type: String, required: true },
 	endedAt: Date,
 }, { timestamps: true }));
