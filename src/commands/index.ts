3a0319a8c7c5992284cfeb2a0a961eaed78152d2
Emily Andrews <emilyandrewsprofessional@gmail.com>
2025-04-03 18:25:56 +0000

    import command in index.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,3 +1,3 @@
 export { default as state } from './chat/state.js';
-
+export { default as scrapeJoinLogs } from './chat/scrapeJoinLogs.js';
 export { default as feadback } from './chat/feedback.js';
