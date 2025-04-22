0a9adc4d1af9fbec6a2e4e1623b4f9875e53dab8
John William Bowen <bowenjw22@outlook.com>
2025-04-21 21:46:31 -0400

    type protection added

--- src/events/guild_member/guildMemberVoiceUpdate.ts
+++ src/events/guild_member/guildMemberVoiceUpdate.ts
@@ -7,7 +7,13 @@ export const guildMemberVoiceUpdate = new Event({
 	name: Events.VoiceStateUpdate,
 	execute: async (oldState, newState) => {
 
-		const {guild, member} = newState
+		if (oldState.channelId === newState.channelId) return
+
+		const {guild} = newState
+		const member = newState.member === null ? await guild.members.fetch(newState.id).catch(console.error) : newState.member;
+
+		if(!member) return
+
 		const settings = await GuildSetting.findOne({guildId: guild.id})
 		// check that logging channel ID is set
 		const loggingChannelId = settings?.logging.voiceUpdatesChannelId
@@ -19,11 +25,11 @@ export const guildMemberVoiceUpdate = new Event({
 
 		let embed:EmbedBuilder
 		if (oldState.channel === null && newState.channel !== null) {
-			embed = vcLogEmbed(member!, 'Joined Voice Channel',`${newState.client.user} joined ${newState.channel}.`,Colors.Green)
+			embed = vcLogEmbed(member, 'Joined Voice Channel',`${member} joined ${newState.channel}.`,Colors.Green)
 		} else if (oldState.channel !== null && newState.channel === null) {
-			embed = vcLogEmbed(member!, 'Left Voice Channel',`${member} left ${oldState.channel}.`, Colors.Red)
+			embed = vcLogEmbed(member, 'Left Voice Channel',`${member} left ${oldState.channel}.`, Colors.Red)
 		} else {
-			embed = vcLogEmbed(member!, 'Switched Voice Channel', `${member} switched from ${oldState.channel} to ${newState.channel}.`, Colors.Blue)
+			embed = vcLogEmbed(member, 'Switched Voice Channel', `${member} switched from ${oldState.channel} to ${newState.channel}.`, Colors.Blue)
 		}
 
 		loggingChannel.send({embeds:[embed]})
@@ -41,9 +47,9 @@ export const guildMemberVoiceUpdate = new Event({
  */
 function vcLogEmbed(member:GuildMember, title:string, description:string, color:ColorResolvable ) {
 	
-	const icon = member?.user.displayAvatarURL({forceStatic:true})
+	const icon = member.user.displayAvatarURL({forceStatic:true})
 	return new EmbedBuilder()
-		.setAuthor({iconURL: icon, name: member?.displayName ?? 'error' })
+		.setAuthor({iconURL: icon, name: member.displayName})
 		.setTitle(title)
 		.setDescription(description)
 		.setTimestamp()
