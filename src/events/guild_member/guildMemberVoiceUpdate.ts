1c11322272ca46034bcc4d16c1106bec90848539
GreatWyrm <alecmusante@gmail.com>
2025-04-21 17:45:54 -0700

    deduplicate code

--- src/events/guild_member/guildMemberVoiceUpdate.ts
+++ src/events/guild_member/guildMemberVoiceUpdate.ts
@@ -6,47 +6,28 @@ import { getGuildChannel } from "../../util/index.js";
 export const guildMemberVoiceUpdate = new Event({
 	name: Events.VoiceStateUpdate,
 	execute: async (oldState, newState) => {
+		const settings = await GuildSetting.findOne({guildId: newState.guild.id})
+		// check that logging channel ID is set
+		const loggingChannelId = settings?.logging.voiceChannelUpdatesId
+		if(!loggingChannelId) return
+
+		// check that logging channel exists in guild
+		const loggingChannel = await getGuildChannel(newState.guild, loggingChannelId)
+		if(!loggingChannel?.isSendable()) return
+
+		const icon = newState.client.user.displayAvatarURL({forceStatic:true})
+
 		if (oldState.channel === null && newState.channel !== null) {
-			const settings = await GuildSetting.findOne({guildId: newState.channel.guild.id})
-			// check that logging channel ID is set
-			const loggingChannelId = settings?.logging.voiceChannelUpdatesId
-			if(!loggingChannelId) return
-
-			// check that logging channel exists in guild
-			const loggingChannel = await getGuildChannel(newState.channel.guild, loggingChannelId)
-			if(!loggingChannel?.isSendable()) return
-
-			const icon = newState.client.user.displayAvatarURL({forceStatic:true})
 			const embed = new EmbedBuilder()
 				.setAuthor({iconURL: icon, name: 'Joined Voice Channel'})
 				.setDescription(`${newState.client.user} joined ${newState.channel}.`)
 				.setColor(Colors.Green)
 		} else if (oldState.channel !== null && newState.channel === null) {
-			const settings = await GuildSetting.findOne({guildId: oldState.channel.guild.id})
-			// check that logging channel ID is set
-			const loggingChannelId = settings?.logging.voiceChannelUpdatesId
-			if(!loggingChannelId) return
-
-			// check that logging channel exists in guild
-			const loggingChannel = await getGuildChannel(oldState.channel.guild, loggingChannelId)
-			if(!loggingChannel?.isSendable()) return
-
-			const icon = newState.client.user.displayAvatarURL({forceStatic:true})
 			const embed = new EmbedBuilder()
 				.setAuthor({iconURL: icon, name: 'Left Voice Channel'})
 				.setDescription(`${oldState.client.user} left ${oldState.channel}.`)
 				.setColor(Colors.Red)
 		} else if (oldState.channel !== null && newState.channel !== null) {
-			const settings = await GuildSetting.findOne({guildId: oldState.channel.guild.id})
-			// check that logging channel ID is set
-			const loggingChannelId = settings?.logging.voiceChannelUpdatesId
-			if(!loggingChannelId) return
-
-			// check that logging channel exists in guild
-			const loggingChannel = await getGuildChannel(oldState.channel.guild, loggingChannelId)
-			if(!loggingChannel?.isSendable()) return
-
-			const icon = newState.client.user.displayAvatarURL({forceStatic:true})
 			const embed = new EmbedBuilder()
 				.setAuthor({iconURL: icon, name: 'Switched Voice Channel'})
 				.setDescription(`${oldState.client.user} switched from ${oldState.channel} to ${newState.channel}.`)
