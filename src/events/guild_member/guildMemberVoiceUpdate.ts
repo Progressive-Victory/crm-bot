61231fe0fc7e70170b9b162241651695fba52dac
John William Bowen <bowenjw22@outlook.com>
2025-04-21 21:14:36 -0400

    reduce repeated code

--- src/events/guild_member/guildMemberVoiceUpdate.ts
+++ src/events/guild_member/guildMemberVoiceUpdate.ts
@@ -1,4 +1,4 @@
-import { Colors, EmbedBuilder, Events } from "discord.js";
+import { ColorResolvable, Colors, EmbedBuilder, Events, GuildMember } from "discord.js";
 import Event from "../../Classes/Event.js";
 import { GuildSetting } from "../../models/Setting.js";
 import { getGuildChannel } from "../../util/index.js";
@@ -6,32 +6,48 @@ import { getGuildChannel } from "../../util/index.js";
 export const guildMemberVoiceUpdate = new Event({
 	name: Events.VoiceStateUpdate,
 	execute: async (oldState, newState) => {
-		const settings = await GuildSetting.findOne({guildId: newState.guild.id})
+
+		const {guild, member} = newState
+		const settings = await GuildSetting.findOne({guildId: guild.id})
 		// check that logging channel ID is set
 		const loggingChannelId = settings?.logging.voiceChannelUpdatesId
 		if(!loggingChannelId) return
 
 		// check that logging channel exists in guild
-		const loggingChannel = await getGuildChannel(newState.guild, loggingChannelId)
+		const loggingChannel = await getGuildChannel(guild, loggingChannelId)
 		if(!loggingChannel?.isSendable()) return
 
-		const icon = newState.client.user.displayAvatarURL({forceStatic:true})
-
+		let embed:EmbedBuilder
 		if (oldState.channel === null && newState.channel !== null) {
-			const embed = new EmbedBuilder()
-				.setAuthor({iconURL: icon, name: 'Joined Voice Channel'})
-				.setDescription(`${newState.client.user} joined ${newState.channel}.`)
-				.setColor(Colors.Green)
+			embed = vcLogEmbed(member!, 'Joined Voice Channel',`${newState.client.user} joined ${newState.channel}.`,Colors.Green)
 		} else if (oldState.channel !== null && newState.channel === null) {
-			const embed = new EmbedBuilder()
-				.setAuthor({iconURL: icon, name: 'Left Voice Channel'})
-				.setDescription(`${oldState.client.user} left ${oldState.channel}.`)
-				.setColor(Colors.Red)
-		} else if (oldState.channel !== null && newState.channel !== null) {
-			const embed = new EmbedBuilder()
-				.setAuthor({iconURL: icon, name: 'Switched Voice Channel'})
-				.setDescription(`${oldState.client.user} switched from ${oldState.channel} to ${newState.channel}.`)
-				.setColor(Colors.Blue)
+			embed = vcLogEmbed(member!, 'Left Voice Channel',`${member} left ${oldState.channel}.`, Colors.Red)
+		} else {
+			embed = vcLogEmbed(member!, 'Switched Voice Channel', `${member} switched from ${oldState.channel} to ${newState.channel}.`, Colors.Blue)
 		}
+
+		loggingChannel.send({embeds:[embed]})
 	}
 })
+
+/**
+ *
+ * @param newSate
+ * @param member
+ * @param title
+ * @param description
+ * @param color
+ * @returns
+ */
+function vcLogEmbed(member:GuildMember, title:string, description:string, color:ColorResolvable ) {
+	
+	const icon = member?.user.displayAvatarURL({forceStatic:true})
+	return new EmbedBuilder()
+		.setAuthor({iconURL: icon, name: member?.displayName ?? 'error' })
+		.setTitle(title)
+		.setDescription(description)
+		.setTimestamp()
+		.setFooter({text:`ID: ${member.id}`})
+		.setColor(color)
+
+}
