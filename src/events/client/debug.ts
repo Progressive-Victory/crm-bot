4cd2738849ee721bcfe7900da4dfcb22ac2244f5
William Bowen <william@progress.win>
2025-05-12 09:10:02 -0400

    clean up debug log

--- src/events/client/debug.ts
+++ src/events/client/debug.ts
@@ -3,8 +3,8 @@ import { Event } from '../../Classes/index.js';
 
 export const debug = new Event({
 	name: Events.Debug,
-	execute: async (info: string) => {
-		if(info.startsWith('[WS => '))
+	execute: async (info) => {
+		if(info.startsWith('[WS => ') || info.startsWith('[object'))
 			return;
 		console.debug(info);
 	}
