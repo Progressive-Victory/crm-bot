d1250755001d11ee0df2c432c01dd624b2d6d020
Marcus Doucette <Marz3587@gmail.com>
2025-05-18 14:09:56 -0400

    feat: Update message format changes
    I made the change mimic the style that was requested.

--- src/events/guild_member/guildMemberUpdate.ts
+++ src/events/guild_member/guildMemberUpdate.ts
@@ -6,26 +6,26 @@ import { getGuildChannel } from "../../util/index.js";
 export const guildMemberUpdate = new Event({
 	name: Events.GuildMemberUpdate,
 	execute: async (oldMember, newMember) => {
-		if(oldMember.pending && oldMember.pending !== newMember.pending) {
-			const {guild} = newMember
-			const settings = await GuildSetting.findOne({guildId: guild.id})
-			
+		if (oldMember.pending && oldMember.pending !== newMember.pending) {
+			const { guild } = newMember
+			const settings = await GuildSetting.findOne({ guildId: guild.id })
+
 			// check that Join channel ID is set
 			const joinChannelId = settings?.welcome.channelId
-			if(!joinChannelId) return
-	
+			if (!joinChannelId) return
+
 			// check that Join channel exists in guild
 			const joinChannel = await getGuildChannel(guild, joinChannelId)
-			if(!joinChannel?.isSendable()) return
-			const icon = newMember.displayAvatarURL({forceStatic:true})
+			if (!joinChannel?.isSendable()) return
+			const icon = newMember.displayAvatarURL({ forceStatic: true })
 			const embed = new EmbedBuilder()
-				.setAuthor({iconURL: icon, name: 'Member Joined'})
+				.setAuthor({ iconURL: icon, name: 'Member Joined' })
 				// .setTitle('Member Joined')
 				.setDescription(`${newMember.toString()} ${newMember.user.username}`)
 				// .setFields({name:'Welcome Message Status', value:inlineCode('not sent')})
 				// .setThumbnail(icon)
 				.setTimestamp(newMember.joinedAt)
-				.setFooter({text: `User ID: ${newMember.user.id}`})
+				.setFooter({ text: `User ID: ${newMember.user.id}` })
 				.setColor(Colors.Blue)
 
 			const welcomeButton = new ButtonBuilder()
@@ -35,34 +35,31 @@ export const guildMemberUpdate = new Event({
 				.setStyle(ButtonStyle.Secondary)
 			const row = new ActionRowBuilder<ButtonBuilder>().addComponents(welcomeButton)
 
-			joinChannel.send({embeds:[embed], components:[row]})
+			joinChannel.send({ embeds: [embed], components: [row] })
 		}
-		if(oldMember.nickname !== newMember.nickname){ 
-			const {guild} = newMember
-			const settings = await GuildSetting.findOne({guildId: guild.id})
+		if (oldMember.nickname !== newMember.nickname) {
+			const { guild } = newMember
+			const settings = await GuildSetting.findOne({ guildId: guild.id })
 
 			const nicknameUpdatesChannelId = settings?.logging.nicknameUpdatesChannelId
-			if(!nicknameUpdatesChannelId) return 
+			if (!nicknameUpdatesChannelId) return
 			let nicknameLogChannel
-			try{
-				nicknameLogChannel = await getGuildChannel(guild,nicknameUpdatesChannelId)
-			}catch(error){
+			try {
+				nicknameLogChannel = await getGuildChannel(guild, nicknameUpdatesChannelId)
+			} catch (error) {
 				return
 			}
-			if(nicknameLogChannel?.isSendable()){
-				const text = new TextDisplayBuilder().setContent(`User "${newMember.user.username}" changed their nickname from ${oldMember.nickname} to ${newMember.nickname}.`)
-				const icon = newMember.displayAvatarURL({forceStatic:true})
-				const content = new ContainerBuilder()
-					.setAccentColor(0x5da4fc)
-					.addSectionComponents(
-						new SectionBuilder()
-						.setThumbnailAccessory(new ThumbnailBuilder().setURL(icon))
-						.addTextDisplayComponents(text)
-					)
-				nicknameLogChannel.send({
-					components:[content],
-					flags:MessageFlags.IsComponentsV2
-				})
+			if (nicknameLogChannel?.isSendable()) {
+				const title = `Nickname Changed`
+				const description = `${newMember} ${newMember.user.username} changed their nickname from ${oldMember.nickname!==null?oldMember.nickname:newMember.user.globalName} to ${newMember.nickname}`
+				const icon = newMember.displayAvatarURL({ forceStatic: true })
+				const embed = new EmbedBuilder()
+					.setAuthor({iconURL: icon, name: title})
+					.setDescription(description)
+					.setTimestamp()
+					.setFooter({text:`User ID: ${newMember.user.id}`})
+					.setColor(2804223)
+				nicknameLogChannel.send({embeds:[embed]})
 			}
 		}
 	}
