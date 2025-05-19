1cc68c5b0faa849515375549a5b00f898c258e3a
William Bowen <william@progress.win>
2025-05-18 20:31:13 -0400

    updated command response

--- src/commands/chat/mute.ts
+++ src/commands/chat/mute.ts
@@ -1,9 +1,20 @@
-import { ChatInputCommandInteraction, 
-	EmbedBuilder, 
-	InteractionContextType, 
-	PermissionFlagsBits, 
-	SlashCommandBuilder, 
-	User} from "discord.js";
+import {
+	ContainerBuilder,
+	EmbedBuilder,
+	Guild,
+	GuildMember,
+	InteractionContextType,
+	MessageFlags,
+	PermissionFlagsBits,
+	PermissionsBitField,
+	SeparatorBuilder,
+	SeparatorSpacingSize,
+	SlashCommandBuilder,
+	subtext,
+	TextDisplayBuilder,
+	time,
+	TimestampStyles
+} from "discord.js";
 import { ChatInputCommand } from "../../Classes/index.js";
 import { GuildSetting } from "../../models/Setting.js";
 import { getGuildChannel } from "../../util/index.js";
@@ -11,11 +22,22 @@ import { getGuildChannel } from "../../util/index.js";
 
 const MUTE_COLOR = 0x7c018c
 
+const durationText = {
+	'3': '3 mins',
+	'10': '10 mins',
+	'30': '30 mins',
+	'60': '1 hour',
+	'360': '6 hours',
+	'1440': '1 Day',
+}
+
+type dTime = '3' | '10' | '30' | '60' | '360' | '1440'
+
 export const mute = new ChatInputCommand({
 	builder: new SlashCommandBuilder()
 		.setName('mute')
 		.setDescription('Commands for muting users')
-		.setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+		.setDefaultMemberPermissions(PermissionFlagsBits.MuteMembers)
 		.setContexts(InteractionContextType.Guild)
 		.addUserOption(option=>option
 			.setName("user")
@@ -41,155 +63,136 @@ export const mute = new ChatInputCommand({
 			.setMaxLength(300)
 			.setRequired(true)
 		),
-	execute: async(interaction)=>{
+	execute: async (interaction)=>{
 		
-		muteUserForTime(interaction)
-		logAction(interaction)
-		dmNotification(interaction)
-		// if user is still in vc
-		vcNotification(interaction)
+		let guild:Guild
+		if (interaction.inCachedGuild()) guild = interaction.guild
+		else if (interaction.inRawGuild()) guild = await interaction.client.guilds.fetch(interaction.guildId)
+		else return
+
+
+		//who are we muting
+		let targetMember = interaction.options.getMember('user')
+		if (!(targetMember instanceof GuildMember)) {
+			targetMember = await guild.members.fetch(interaction.options.getUser('user', true))
+		}
+
+		// check that target is not a admin or bot
+		if (targetMember.user.bot || targetMember.permissions.has(PermissionsBitField.Flags.Administrator)) {
+			void interaction.reply({
+				content: `${targetMember.toString()} bots and admins can not be server muted`,
+				flags: MessageFlags.Ephemeral
+			})
+			return
+		}
+
+		// Muting member
+		let mutingMember = interaction.member
+		if (!(mutingMember instanceof GuildMember)) {
+			mutingMember = await guild.members.fetch(interaction.user)
+		}
+		
+		// and for how long
+		const durationMinutes = interaction.options.getInteger('duration', true)
+		const reason = interaction.options.getString('reason', true)
+
+		// set mute to true
+		targetMember.voice.setMute(true, reason)
+
+		// set timeout to revert mute
+		setTimeout( () => {
+			if (targetMember.voice.serverMute)
+				targetMember.voice.setMute(false, "Mute Time Elapsed")
+		}, durationMinutes*60000)
+		
+		const endDate = new Date(new Date().getTime() + durationMinutes*60000)
+
+		// Message to be sent to channels
+		vcMessage(targetMember, mutingMember, durationMinutes)
+		logMessage(targetMember, mutingMember, durationMinutes, reason)
+
+		interaction.reply({
+			content:`${targetMember.toString()} has been server muted. They will be unmuted ${time(endDate, TimestampStyles.RelativeTime)}`,
+			flags: MessageFlags.Ephemeral
+		})
 	}
 })
 
-
-/**
- * mute the user for the specified time
- * @param interaction command interaction from user
- */
-function muteUserForTime(interaction:ChatInputCommandInteraction){
-	//who are we muting
-	const member = interaction.guild?.members.cache.get(
-		interaction.options.getUser('user')?.id??""
-	)
-	// and for how long
-	const durationMinutes = interaction.options.getInteger('duration') ?? 0
-	const reason = interaction.options.getString('reason') ?? "Because an admin said so."
-	
-	// set mute to true
-	member?.voice.setMute(true,reason)
-	
-	// set timeout to revert mute
-	setTimeout(()=>{
-		member?.voice.setMute(false,"Mute Time Elapsed")
-	},durationMinutes*60000)
-}
-
-
 /**
  * log the mute in specified logging server
- * @param interaction command interaction from user
+ * @param targetMember The member who was muted
+ * @param mutingMember The member who muted targetMember
+ * @param durationMinutes number representing the number minutes targetMember is muted for
+ * @param reason Why the targetMember was muted
  */
-async function logAction(interaction:ChatInputCommandInteraction){
-	const {
-		options,guild,user:muter
-	} = interaction
-	const settings = await GuildSetting.findOne({guildId:guild?.id})
-	if (!settings?.logging.timeoutChannelId||!guild) return
-	
-	const mutedUser = options.getUser('user');
-	const durationMinutes = options.getInteger('duration')
-	const reason = options.getString('reason')
-	if(mutedUser===null || durationMinutes===null || reason === null) return
+async function logMessage(targetMember:GuildMember, mutingMember:GuildMember, durationMinutes:number, reason:string ){
 
-	const timeoutChannel = await getGuildChannel(guild, settings.logging.timeoutChannelId)
+	// check if log channel is set
+	const settings = await GuildSetting.findOne({guildId: targetMember.guild.id})
+	if (!settings?.logging.timeoutChannelId) return
+
+	// check that channel is real
+	const timeoutChannel = await getGuildChannel(targetMember.guild, settings.logging.timeoutChannelId)
 	if(!timeoutChannel?.isSendable()) return
 
-	timeoutChannel.send({embeds:[getMuteLogEmbed(muter,mutedUser,durationMinutes,reason)]})
+	const title = "User Muted"
+	const description = `${targetMember.toString()} was muted by ${mutingMember.toString()}`
+	const avatarURL = targetMember.displayAvatarURL({ forceStatic:true })
+
+	const embed = new EmbedBuilder()
+		.setAuthor({iconURL:avatarURL, name:title})
+		.setDescription(description)
+		.addFields(
+			{name:'Duration', value: `${durationMinutes} minutes` },
+			{ name: 'Reason', value: reason }
+		)
+		.setTimestamp()
+		.setFooter({text:`User ID: ${targetMember.id}`})
+		.setColor(MUTE_COLOR)
+	
+	// send to channel
+	timeoutChannel.send({ embeds:[embed] })
 }
 
-/**
- * send a dm to the user informing them of why they were muted
- * @param interaction command interaction from user
- */
-function dmNotification(interaction:ChatInputCommandInteraction){
-	const mutedUser = interaction.options.getUser('user')
-	const botIcon = interaction.client.user.displayAvatarURL({forceStatic:true})
-	let durationMinutes = interaction.options.getInteger('duration')
-	let durationHours = 0
-	let durationDays = 0
-	if (mutedUser===null || durationMinutes===null) return
-	while (durationMinutes>=60){
-		durationMinutes-=60
-		durationHours+=1
-	}
-	while (durationHours>=24){
-		durationHours-=24
-		durationDays+=1
-	}
-	mutedUser.send({embeds:[getMuteNotificationEmbed(botIcon,`You were muted`,durationMinutes,durationHours,durationDays)]})
-}
+// /**
+//  * send a dm to the user informing them of why they were muted
+//  * @param interaction command interaction from user
+//  * @param mutedMember
+//  * @param durationMinutes
+//  */
+// // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// function dmNotification(mutedMember:GuildMember, durationMinutes:number ){
+// 	const mutedMember = mutedMember.guild.iconURL({forceStatic: true}) ?? undefined
+// 	const endDate = new Date(new Date().getTime() + durationMinutes*60000)
+// 	mutedMember.send({embeds:[getMuteNotificationEmbed(botIcon,`You were muted`, durationMinutes, durationHours, durationDays)]})
+// }
 
 /**
  * send a notification to the voice chat for the current voice server
- * @param interaction command interaction from user
+ * @param targetMember The member who was muted
+ * @param mutingMember The member who muted targetMember
+ * @param durationMinutes number representing the number minutes targetMember is muted for
  */
-function vcNotification(interaction:ChatInputCommandInteraction){
-	const mutedUser = interaction.options.getUser('user')
-	const botIcon = interaction.client.user.displayAvatarURL({forceStatic:true})
+function vcMessage(targetMember:GuildMember, mutingMember:GuildMember, durationMinutes:number){
+	// check if member is connected to channel
+	const channel = targetMember.voice.channel
+	if (!channel) return
 
-	if (mutedUser===null) return
-
-	const mutedMember = interaction.guild?.members.cache.get(
-		mutedUser.id
-	)
-	const adminMember = interaction.guild?.members.cache.get(
-		interaction.user.id
-	)
-	const mutedVC = mutedMember?.voice.channel??null
-	const adminVC = adminMember?.voice.channel
+	const text = new TextDisplayBuilder().setContent([
+		`${targetMember.toString()} was muted for ${durationText[durationMinutes.toString() as dTime]}`
+	].join('\n'))
+	const separator = new SeparatorBuilder().setDivider(true).setSpacing(SeparatorSpacingSize.Small)
+	const footer = new TextDisplayBuilder().setContent(subtext(`User ID: ${targetMember.id}`))
 	
-	if (mutedVC!==adminVC||mutedVC===null) return
-
-	let durationMinutes = interaction.options.getInteger('duration')
-	let durationHours = 0
-	let durationDays = 0
-	if (durationMinutes===null) return
-	while (durationMinutes>=60){
-		durationMinutes-=60
-		durationHours+=1
-	}
-	while (durationHours>=24){
-		durationHours-=24
-		durationDays+=1
-	}
-	mutedVC.send({embeds:[getMuteNotificationEmbed(botIcon,`${mutedUser} was muted`,durationMinutes,durationHours,durationDays)]})
-}
-
-/**
- * construct the embed for a mute log
- * @param muter admin who is muting
- * @param user who is muted
- * @param durationMinutes how long are they muted for?
- * @param reason why were they muted?
- */
-function getMuteLogEmbed(muter:User,user:User,durationMinutes:number,reason:string){
-	const title = "User Muted"
-	const description = `${user} was muted by ${muter} for ${durationMinutes} minutes because ${reason}`
-	const icon = user.displayAvatarURL({forceStatic:true})
-	return new EmbedBuilder()
-		.setAuthor({iconURL:icon, name:title})
-		.setDescription(description)
-		.setTimestamp()
-		.setFooter({text:`User ID: ${user.id}`})
-		.setColor(MUTE_COLOR)
-}
-
-/**
- * construct the embed for a mute notification
- * @param iconURL url for icon of notification
- * @param address Short message to address message to whomever is going to see it
- * @param durationMinutes how long was the mute
- */
-function getMuteNotificationEmbed(iconURL:string,address:string,durationMinutes:number,durationHours:number=0,durationDays:number=0){
-	const title = "User Muted"
-	const days = durationDays>0?`${durationDays} days `:""
-	const hours = durationHours>0||durationDays>0?`${durationHours} hours and `:""
-	const minutes = `${durationMinutes} minutes`
-	const description = `${address} for ${days}${hours}${minutes}`
-	return new EmbedBuilder()
-		.setAuthor({iconURL:iconURL,name:title})
-		.setDescription(description)
-		.setTimestamp()
-		.setColor(MUTE_COLOR)
+	const container = new ContainerBuilder()
+		.addTextDisplayComponents(text)
+		.addSeparatorComponents(separator)
+		.addTextDisplayComponents(footer)
+		.setAccentColor(MUTE_COLOR)
+	
+	channel.send({
+		components:[container],
+		flags:MessageFlags.IsComponentsV2
+	})
 }
 
